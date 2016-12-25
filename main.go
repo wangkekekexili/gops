ff11e51f7c21aba56da6d7ac37dd041138ccc958
Ke Wang <wangkekekexili@gmail.com>
2016-12-25 08:21:47 -0500

    Use zap structured logging.

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/uber-go/zap"
 	"github.com/wangkekekexili/gops/model"
 	"github.com/wangkekekexili/gops/util"
 	"gopkg.in/mgo.v2"
 )
 
 func main() {
+	// Config logger.
+	logger := zap.New(zap.NewJSONEncoder(zap.NoTime()))
+
 	// Connect to mlab mongodb.
 	uri, err := util.BuildMongodbURI()
 	if err != nil {
-		panic(err)
+		logger.Fatal("Cannot build MongoDB URI.", zap.String("message", err.Error()))
+		return
 	}
 	session, err := mgo.Dial(uri)
 	if err != nil {
-		panic(err)
+		logger.Fatal("Cannot connect to MongoDB.", zap.String("message", err.Error()))
 	}
 	defer session.Close()
 
@@ -24,19 +27,22 @@ func main() {
 	gamestop := &gops.Gamestop{}
 	gamesToInsert, gamesToUpdate, err := gamestop.GetGamesInfo(c)
 	if err != nil {
-		panic(err)
+		logger.Error("Unable to get games info.", zap.String("source", gops.ProductSourceGamestop))
 	}
 	if len(gamesToInsert) > 0 {
-		fmt.Printf("inserting %d documents\n", len(gamesToInsert))
+		logger.Info("Inserting documents.", zap.Int("number", len(gamesToInsert)))
 		if err = c.Insert(gamesToInsert...); err != nil {
-			panic(err)
+			logger.Warn("Unable to insert games.")
 		}
 	}
 	if len(gamesToUpdate) > 0 {
-		fmt.Printf("updating %d documents\n", len(gamesToUpdate))
+		logger.Info("Updating documents.", zap.Int("number", len(gamesToUpdate)))
 		for objectID, game := range gamesToUpdate {
 			if err = c.UpdateId(objectID, game); err != nil {
-				fmt.Println(err)
+				logger.Warn("Unable to update a document.",
+					zap.String("id", objectID.Hex()),
+					zap.Object("game", game),
+				)
 			}
 		}
 	}
